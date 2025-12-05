088a26ec19401ec7bbd37f16f314f80b28699e14
jmgrndos <jmgranadosgo@gmail.com>
2025-12-05 12:41:30 -0500

    chore: project init

---
+++ src/main/java/showtime_corp/profile_vitaile/ProfileVitaileApplication.java
@@ -0,0 +1,13 @@
+package showtime_corp.profile_vitaile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileVitaileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileVitaileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/showtime_corp/profile_vitaile/ProfileVitaileApplicationTests.java
@@ -0,0 +1,13 @@
+package showtime_corp.profile_vitaile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileVitaileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
