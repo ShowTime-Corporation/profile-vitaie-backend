c473b3738bb84a4c90aa9efd88f4797100748aae
SantiagoToroMesa <storomes@gmail.com>
2025-12-06 21:05:41 -0500

    fix: controller correction and filter configurtion

--- src/main/java/showtime_corp/profile_vitaile/controller/AuthController.java
+++ src/main/java/showtime_corp/profile_vitaile/controller/AuthController.java
@@ -5,10 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import showtime_corp.profile_vitaile.dto.RegisterRequest;
 import showtime_corp.profile_vitaile.entity.User;
 import showtime_corp.profile_vitaile.service.AuthService;
 
+@RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
